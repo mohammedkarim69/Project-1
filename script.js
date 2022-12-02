c6815f44ab262399270185796081a32ac0a1d4dd
mohammedkarim69! <mohammed.karim.bouzidi@gmail.com>
2022-12-03 00:11:58 +0100

    started mediaquery

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const entree = document.querySelector(".img1")
-const plats = document.querySelector(".img2")
-const dessert = document.querySelector(".img3")
-const hoverentree = document.querySelector(".hoverentree")
-console.log(hoverentree);
-const hoverplats = document.querySelector(".hoverplats")
-const hoverdessert = document.querySelector(".hoverdessert")
+const entree = document.querySelector(".img1");
+const plats = document.querySelector(".img2");
+const dessert = document.querySelector(".img3");
+const hoverentree = document.querySelector(".hoverentree");
+const hoverplats = document.querySelector(".hoverplats");
+const hoverdessert = document.querySelector(".hoverdessert");
 
-    entree.addEventListener("mouseover", () => {
-        hoverentree.classList.add("apparition");
+
+entree.addEventListener("mouseover", () => {
+    hoverentree.classList.add("apparition");
 
     });
 
@@ -26,3 +26,12 @@ dessert.addEventListener("mouseover", () => {
 
 });
 
+
+const icon = document.querySelector(".fa-bars");
+const list = document.querySelector(".navlistmobile");
+
+icon.addEventListener("click", () => {   
+list.classList.toggle("list-visible");
+icon.classList.toggle("icon-visible");
+
+});
